f341fd9db4d097af235641f511f5d1714976b854
BeSaRa <besara@windowslive.com>
2023-03-12 13:44:18 +0300

    chore: update the structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import '@utils/protoypes/custom-prototypes';
-import { LoginComponent } from '@modules/app/components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '@modules/shared/shared.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
