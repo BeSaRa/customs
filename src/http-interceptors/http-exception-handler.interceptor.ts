98960fe89729539df7dd2fa8364d42f25cb6558c
BeSaRa <besara@windowslive.com>
2023-04-04 12:02:44 +0300

    Http exception Handler
    Signed-off-by: BeSaRa <besara@windowslive.com>

--- src/http-interceptors/http-exception-handler.interceptor.ts
+++ src/http-interceptors/http-exception-handler.interceptor.ts
@@ -1,20 +1,26 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import {
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
+import { ExceptionHandlerService } from '@services/exception-handler.service';
 
 @Injectable()
 export class HttpExceptionHandlerInterceptor implements HttpInterceptor {
-  constructor() {}
+  private readonly handler = inject(ExceptionHandlerService);
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((err) => {
+        this.handler.httpExceptionHandle(err);
+        return throwError(err);
+      })
+    );
   }
 }
