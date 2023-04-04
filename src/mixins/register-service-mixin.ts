84dbd600f0f432f4b091e3cf9b8b76b206c56910
BeSaRa <besara@windowslive.com>
2023-04-04 11:05:04 +0300

    create register service mixin
    Signed-off-by: BeSaRa <besara@windowslive.com>

--- src/mixins/register-service-mixin.ts
+++ src/mixins/register-service-mixin.ts
@@ -1,10 +1,12 @@
 import { AbstractConstructor, Constructor } from '@app-types/constructors';
 import { ServiceRegistry } from '@services/service-registry';
 
-export function BaseServiceMixin<T extends AbstractConstructor<object>>(
+export function RegisterServiceMixin<T extends AbstractConstructor<object>>(
   base: T
 ): T;
-export function BaseServiceMixin<T extends Constructor<object>>(base: T): T {
+export function RegisterServiceMixin<T extends Constructor<object>>(
+  base: T
+): T {
   return class BaseServiceMixin extends base {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     constructor(...args: any[]) {
