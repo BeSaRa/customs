e956d9d2b16efee645d335ede1b1528da956a4c7
BeSaRa <besara@windowslive.com>
2023-04-04 12:15:06 +0300

    plug AuthGuard to LoginComponent
    Signed-off-by: BeSaRa <besara@windowslive.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { authGuard } from '@guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+    canActivate: [authGuard('GUEST')],
   },
 ];
 
