c9956e5d00ca88109dd8e95c71422363b724d6b9
BeSaRa <besara@windowslive.com>
2023-04-04 11:03:52 +0300

    create custom operator to ignore the errors that came from Observable
    Signed-off-by: BeSaRa <besara@windowslive.com>

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import { FormControlDirective, FormControlName, NgModel } from '@angular/forms';
+import { catchError, MonoTypeOperatorFunction, NEVER, Observable } from 'rxjs';
 
 export function isNgModel(control: unknown): control is NgModel {
   return control instanceof NgModel;
@@ -14,4 +15,15 @@ export function isFormControlName(
   control: unknown
 ): control is FormControlName {
   return control instanceof FormControlName;
+}
+
+export function ignoreErrors<T>(debug = false): MonoTypeOperatorFunction<T> {
+  return (source: Observable<T>) => {
+    return source.pipe(
+      catchError((error) => {
+        debug && console.log(error);
+        return NEVER;
+      })
+    );
+  };
 }
