7b9b5eac531c940c8a65569de8bc81d91a9e0805
BeSaRa <besara@windowslive.com>
2023-04-04 12:13:43 +0300

    permission Model
    Signed-off-by: BeSaRa <besara@windowslive.com>

--- src/models/permission.ts
+++ src/models/permission.ts
@@ -1,10 +1,10 @@
 import { ClonerMixin } from '@mixins/cloner-mixin';
-import { LangMixin } from '@mixins/lang-mixin';
+import { GetNamesMixin } from '@mixins/get-names-mixin';
 import { GetNamesContract } from '@contracts/get-names-contract';
 import { CloneContract } from '@contracts/clone-contract';
 
 export class Permission
-  extends LangMixin(ClonerMixin(class {}))
+  extends GetNamesMixin(ClonerMixin(class {}))
   implements GetNamesContract, CloneContract
 {
   id!: number;
