0364bf7fb9c1e9e549608e71f8ac3c30eb101a82
BeSaRa <besara@windowslive.com>
2023-04-13 11:53:27 +0300

    update app icons
    Signed-off-by: BeSaRa <besara@windowslive.com>

--- src/enums/app-icons.ts
+++ src/enums/app-icons.ts
@@ -1,6 +1,6 @@
 export enum AppIcons {
   RELOAD = 'reload',
-  DOMAIN = 'domain',
-  WRENCH_CLOCK = 'wrench-clock',
   DOTS_CIRCLE = 'dots-circle',
+  TRANSLATE = 'translate',
+  SETTINGS = 'cog',
 }
