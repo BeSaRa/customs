f187c09374532dec81a08f78fa2ed6c6dacf8907
BeSaRa <ahmedbesara@gmailcom>
2023-04-18 01:57:39 +0300

    add input to the top-navbar

--- src/standalone/components/navbar/navbar.component.ts
+++ src/standalone/components/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { InputComponent } from '@standalone/components/input/input.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, InputComponent],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
