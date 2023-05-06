f50d4c3faca9e91ee98efcc5f4a1523190536c5f
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-05-06 10:43:54 -0700

    before filter standalone

--- src/models/team.ts
+++ src/models/team.ts
@@ -8,7 +8,6 @@ const { send, receive } = new TeamInterceptor();
 @InterceptModel({ send, receive })
 export class Team extends BaseModel<Team, TeamService> {
   $$__service_name__$$ = 'TeamService';
-  status!: number | null;
 
   buildForm(controls = false): object {
     return {};
