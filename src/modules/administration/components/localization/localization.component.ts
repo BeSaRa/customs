983120308eb928154acb07bbc4765590179d3f5d
BeSaRa <ahmedbesara@gmailcom>
2023-05-10 11:51:52 +0300

    remove the ViewChild trigger from localization component

--- src/modules/administration/components/localization/localization.component.ts
+++ src/modules/administration/components/localization/localization.component.ts
@@ -1,28 +1,21 @@
-import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AdminComponent } from '@abstracts/admin-component';
 import { Localization } from '@models/localization';
 import { LocalizationService } from '@services/localization.service';
 import { LocalizationPopupComponent } from '@modules/administration/popups/localization-popup/localization-popup.component';
 import { ContextMenuActionContract } from '@contracts/context-menu-action-contract';
 import { AppIcons } from '@constants/app-icons';
-import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-localization',
   templateUrl: './localization.component.html',
   styleUrls: ['./localization.component.scss'],
 })
-export class LocalizationComponent
-  extends AdminComponent<
-    LocalizationPopupComponent,
-    Localization,
-    LocalizationService
-  >
-  implements AfterViewInit
-{
-  ngAfterViewInit(): void {
-    console.log(this.trigger);
-  }
+export class LocalizationComponent extends AdminComponent<
+  LocalizationPopupComponent,
+  Localization,
+  LocalizationService
+> {
   service = inject(LocalizationService);
   displayedColumns: string[] = [
     'select',
@@ -60,7 +53,4 @@ export class LocalizationComponent
       },
     },
   ];
-
-  @ViewChild('subTrigger', { read: MatMenuTrigger })
-  trigger!: MatMenuTrigger;
 }
