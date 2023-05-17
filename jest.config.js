15dbfd7376574ae88962af9ee03c670e27dab854
BeSaRa <ahmedbesara@gmailcom>
2023-05-17 14:39:45 +0300

    test update

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ const { pathsToModuleNameMapper } = require("ts-jest");
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
 
+
+
   preset: "jest-preset-angular",
   roots: ["<rootDir>/src/"],
   testMatch: ["**/+(*.)+(spec).+(ts)"],
