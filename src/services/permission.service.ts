3dc3a38f5d9e9db74246cc9e567253e4623f6839
aalshahin <aalshahin@eblacorp.com>
2023-05-21 14:27:41 +0400

    using responseContract

--- src/services/permission.service.ts
+++ src/services/permission.service.ts
@@ -4,8 +4,7 @@ import { Permission } from '@models/permission';
 import { CastResponse, CastResponseContainer } from 'cast-response';
 import { map, Observable, tap } from 'rxjs';
 import { Constructor } from '@app-types/constructors';
-
-interface savePermissionsResult {count:number, sc: number, rs: number[]}
+import { ResponseContract } from '@contracts/response-contract';
 
 @CastResponseContainer({
   $default: {
@@ -36,14 +35,14 @@ export class PermissionService extends BaseCrudService<Permission> {
   private _savePermissions(
     userId: number,
     permissions: number[]
-  ): Observable<savePermissionsResult> {
-    return this.http.post<savePermissionsResult>(
+  ): Observable<ResponseContract<number>> {
+    return this.http.post<ResponseContract<number>>(
       this.getUrlSegmentUserPreferences() + '/' + userId + '/bulk',
       permissions
     );
   }
 
-  savePermissions(userId: number, permissions: number[]): Observable<savePermissionsResult> {
+  savePermissions(userId: number, permissions: number[]): Observable<ResponseContract<number>> {
     return this._savePermissions(userId, permissions);
   }
 
