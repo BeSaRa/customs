ab541361ba793274b28b5822ffdd8c8364a5baad
Modar Eskandar <modar.a.eskandar@gmail.com>
2023-05-23 19:30:08 +0300

    remove delete action from context menu in organization units

--- src/modules/administration/components/organization-unit/organization-unit.component.ts
+++ src/modules/administration/components/organization-unit/organization-unit.component.ts
@@ -17,11 +17,7 @@ import { InternalUserService } from '@services/internal-user.service';
   templateUrl: './organization-unit.component.html',
   styleUrls: ['./organization-unit.component.scss'],
 })
-export class OrganizationUnitComponent extends AdminComponent<
-  OrganizationUnitPopupComponent,
-  OrganizationUnit,
-  OrganizationUnitService
-> {
+export class OrganizationUnitComponent extends AdminComponent<OrganizationUnitPopupComponent, OrganizationUnit, OrganizationUnitService> {
   service = inject(OrganizationUnitService);
 
   internalUserService = inject(InternalUserService);
@@ -31,7 +27,7 @@ export class OrganizationUnitComponent extends AdminComponent<
       type: 'action',
       label: 'view',
       icon: AppIcons.VIEW,
-      callback: (item) => {
+      callback: item => {
         this.view$.next(item);
       },
     },
@@ -40,17 +36,8 @@ export class OrganizationUnitComponent extends AdminComponent<
       type: 'action',
       label: 'edit',
       icon: AppIcons.EDIT,
-      callback: (item) => {
+      callback: item => {
         this.edit$.next(item);
-      },
-    },
-    {
-      name: 'delete',
-      type: 'action',
-      label: 'delete',
-      icon: AppIcons.DELETE,
-      callback: (item) => {
-        this.delete$.next(item);
       },
     },
     {
@@ -62,21 +49,19 @@ export class OrganizationUnitComponent extends AdminComponent<
     {
       name: 'email',
       type: 'info',
-      label: (item) => `${this.lang.map.email} : ${item.email}`,
+      label: item => `${this.lang.map.email} : ${item.email}`,
       parent: 'more-details',
     },
     {
       name: 'ldap_group_name',
       type: 'info',
-      label: (item) =>
-        `${this.lang.map.ldap_group_name} : ${item.ldapGroupName}`,
+      label: item => `${this.lang.map.ldap_group_name} : ${item.ldapGroupName}`,
       parent: 'more-details',
     },
     {
       name: 'main_team',
       type: 'info',
-      label: (item) =>
-        `${this.lang.map.main_team} : ${item.mainTeam.getNames()}`,
+      label: item => `${this.lang.map.main_team} : ${item.mainTeam.getNames()}`,
       parent: 'more-details',
     },
   ];
@@ -86,30 +71,13 @@ export class OrganizationUnitComponent extends AdminComponent<
     new TextFilterColumn('arName'),
     new TextFilterColumn('enName'),
     new TextFilterColumn('code'),
-    new SelectFilterColumn(
-      'type',
-      this.lookupService.lookups.organizationUniType,
-      'lookupKey',
-      'getNames'
-    ),
+    new SelectFilterColumn('type', this.lookupService.lookups.organizationUniType, 'lookupKey', 'getNames'),
     // for parent
-    new SelectFilterColumn(
-      'parent',
-      this.service.loadAsLookups(),
-      'id',
-      'getNames'
-    ),
-    new SelectFilterColumn(
-      'managerId',
-      this.internalUserService.loadAsLookups(),
-      'id',
-      'getNames'
-    ),
+    new SelectFilterColumn('parent', this.service.loadAsLookups(), 'id', 'getNames'),
+    new SelectFilterColumn('managerId', this.internalUserService.loadAsLookups(), 'id', 'getNames'),
     new SelectFilterColumn(
       'status',
-      this.lookupService.lookups.commonStatus.filter(
-        (item) => item.lookupKey !== StatusTypes.DELETED
-      ),
+      this.lookupService.lookups.commonStatus.filter(item => item.lookupKey !== StatusTypes.DELETED),
       'lookupKey',
       'getNames'
     ),
