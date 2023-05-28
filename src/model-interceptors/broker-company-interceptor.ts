a9c3ee45891d16c08eaa4a37d70ef28860a8857d
aalshahin <aalshahin@eblacorp.com>
2023-05-28 13:29:23 +0400

    broker company

--- src/model-interceptors/broker-company-interceptor.ts
+++ src/model-interceptors/broker-company-interceptor.ts
@@ -1,5 +1,6 @@
 import { ModelInterceptorContract } from 'cast-response';
 import { BrokerCompany } from '@models/broker-company';
+import { AdminResult } from '@models/admin-result';
 
 export class BrokerCompanyInterceptor implements ModelInterceptorContract<BrokerCompany> {
   send(model: Partial<BrokerCompany>): Partial<BrokerCompany> {
@@ -9,6 +10,7 @@ export class BrokerCompanyInterceptor implements ModelInterceptorContract<Broker
   receive(model: BrokerCompany): BrokerCompany {
     model.licenseStartDate = new Date(model.licenseStartDate).toDateString();
     model.licenseEndDate = new Date(model.licenseEndDate).toDateString();
+    model.statusInfo && (model.statusInfo = AdminResult.createInstance(model.statusInfo));
 
     return model;
   }
