5761e986311eeb77a57ec40aed7a2c44f0eb58af
MHM-86 <mohammad.mousa.it@gmail.com>
2023-06-13 02:39:30 +0300

    - Use language service to provide translation for validation error messages.

--- src/constants/validation-messages.ts
+++ src/constants/validation-messages.ts
@@ -2,6 +2,8 @@ import { ValidationMessageContract } from '@contracts/validation-message-contrac
 
 export const ValidationMessages: Record<string, ValidationMessageContract> = {
   required: { key: 'required_field' },
+  AR_NUM: { key: 'only_arabic_letters_and_numbers' },
+  ENG_NUM: { key: 'only_english_letters_and_numbers' },
 };
 
 export type ValidationMessagesType = typeof ValidationMessages;
