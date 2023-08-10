8c8ec4ffdfd5416f3a0a2647cb9be9521d79a16d
aalshahin <aalshahin@eblacorp.com>
2023-08-10 13:20:25 +0400

    finishing penalty basic info

--- src/modules/administration/popups/penalty-popup/penalty-popup.component.ts
+++ src/modules/administration/popups/penalty-popup/penalty-popup.component.ts
@@ -3,12 +3,13 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CrudDialogDataContract } from '@contracts/crud-dialog-data-contract';
 import { Penalty } from '@models/penalty';
 import { AdminDialogComponent } from '@abstracts/admin-dialog-component';
-import { UntypedFormGroup } from '@angular/forms';
+import { FormGroup, UntypedFormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { OperationType } from '@enums/operation-type';
 import { LookupService } from '@services/lookup.service';
 import { Lookup } from '@models/lookup';
 import { StatusTypes } from '@enums/status-types';
+import { CustomValidators } from '@validators/custom-validators';
 
 @Component({
   selector: 'app-penalty-popup',
@@ -21,7 +22,7 @@ export class PenaltyPopupComponent extends AdminDialogComponent<Penalty> {
   form!: UntypedFormGroup;
   data: CrudDialogDataContract<Penalty> = inject(MAT_DIALOG_DATA);
 
-  penaltyTypes: Lookup[] = this.lookupService.lookups.penaltyType;
+  offenderTypes: Lookup[] = this.lookupService.lookups.offenderType;
 
   statusTooltipText = this.model?.status === StatusTypes.ACTIVE ? this.lang.map.active : this.lang.map.in_active;
 
@@ -32,6 +33,8 @@ export class PenaltyPopupComponent extends AdminDialogComponent<Penalty> {
   protected override _afterBuildForm(): void {
     super._afterBuildForm();
     this.listenToStatusChange();
+    this.listenToIsCash();
+    this.listenToIsDeduction();
   }
 
   protected _beforeSave(): boolean | Observable<boolean> {
@@ -60,7 +63,37 @@ export class PenaltyPopupComponent extends AdminDialogComponent<Penalty> {
     });
   }
 
+  listenToIsCash() {
+    this.form.get('isCash')?.valueChanges.subscribe(value => {
+      value
+        ? this.cashAmountField?.addValidators([CustomValidators.required, CustomValidators.number, Validators.min(500), Validators.max(5000)])
+        : this.cashAmountField?.clearValidators();
+      this.cashAmountField?.updateValueAndValidity();
+    });
+  }
+
+  listenToIsDeduction() {
+    this.form.get('isDeduction')?.valueChanges.subscribe(value => {
+      value
+        ? this.deductionDaysField?.addValidators([CustomValidators.required, CustomValidators.number, Validators.min(1), Validators.max(15)])
+        : this.deductionDaysField?.clearValidators();
+      this.deductionDaysField?.updateValueAndValidity();
+    });
+  }
+
+  get cashAmountField() {
+    return this.form.get('cashAmount');
+  }
+  get deductionDaysField() {
+    return this.form.get('deductionDays');
+  }
   get status() {
     return this.form.get('status');
+  }
+  get isDeductionTrue() {
+    return this.form.get('isDeduction')?.value === 1;
+  }
+  get isCashTrue() {
+    return this.form.get('isCash')?.value === 1;
   }
 }
