7f2244eb1d0c598395679142e73487733ad34d65
BeSaRa <ahmedbesara@gmailcom>
2023-09-27 02:09:02 +0300

    create case-attachment model
    Signed-off-by: BeSaRa <ahmedbesara@gmailcom>

--- src/models/case-attachment.ts
+++ src/models/case-attachment.ts
@@ -1,1 +1,26 @@
-export class CaseAttachment {}
+import { AdminResult } from '@models/admin-result';
+import { ClonerMixin } from '@mixins/cloner-mixin';
+
+export class CaseAttachment extends ClonerMixin(class {}) {
+  id!: string;
+  createdBy!: string;
+  createdOn!: Date | string;
+  lastModified!: Date | string;
+  lastModifier!: string;
+  classDescription!: string;
+  creatorInfo!: AdminResult;
+  ouInfo!: AdminResult;
+  mimeType!: string;
+  documentTitle!: string;
+  contentSize!: number;
+  minorVersionNumber!: number;
+  majorVersionNumber!: number;
+  vsId!: string;
+  versionStatus!: number;
+  isCurrent!: true;
+  lockTimeout!: string;
+  lockOwner!: string;
+  attachmentTypeId!: number;
+  description!: string;
+  attachmentTypeInfo!: AdminResult;
+}
