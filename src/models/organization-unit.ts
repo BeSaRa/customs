3dd1875aeb1f3458439657e5819ddb5b886b7cba
aalshahin <aalshahin@eblacorp.com>
2023-09-26 16:00:27 +0400

    adding mawared dep

--- src/models/organization-unit.ts
+++ src/models/organization-unit.ts
@@ -3,7 +3,6 @@ import { OrganizationUnitService } from '@services/organization-unit.service';
 import { OrganizationUnitInterceptor } from '@model-interceptors/organization-unit-interceptor';
 import { InterceptModel } from 'cast-response';
 import { AdminResult } from './admin-result';
-import { Team } from './team';
 import { CustomValidators } from '@validators/custom-validators';
 import { StatusTypes } from '@enums/status-types';
 
@@ -15,18 +14,17 @@ export class OrganizationUnit extends BaseModel<OrganizationUnit, OrganizationUn
   override status = StatusTypes.ACTIVE;
 
   type!: number;
-  typeInfo!: AdminResult;
-
   code!: string;
   email!: string;
   ldapGroupName!: string;
   ldapLimitedGroupName!: string;
-
   managerId!: number;
-  managerInfo!: AdminResult;
-
   parent!: number;
+  mawaredDepId!: number;
+  typeInfo!: AdminResult;
   parentInfo!: AdminResult;
+  managerInfo!: AdminResult;
+  mawaredDepInfo!: AdminResult;
 
   buildForm(controls = false): object {
     const { arName, enName, code, email, type, managerId, ldapGroupName, ldapLimitedGroupName, parent, status } = this;
