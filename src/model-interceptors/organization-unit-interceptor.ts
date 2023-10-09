4e99a37fc737043e090dca2aaaa7ffc01da9b68a
aalshahin <aalshahin@eblacorp.com>
2023-10-09 16:34:22 +0400

    fixing Bug 58818

--- src/model-interceptors/organization-unit-interceptor.ts
+++ src/model-interceptors/organization-unit-interceptor.ts
@@ -4,14 +4,21 @@ import { AdminResult } from '@models/admin-result';
 
 export class OrganizationUnitInterceptor implements ModelInterceptorContract<OrganizationUnit> {
   send(model: Partial<OrganizationUnit>): Partial<OrganizationUnit> {
+    delete model.statusInfo;
+    delete model.managerInfo;
+    delete model.typeInfo;
+    delete model.parentInfo;
+    delete model.mawaredDepInfo;
+
     return model;
   }
 
   receive(model: OrganizationUnit): OrganizationUnit {
-    model.statusInfo = new AdminResult().clone(model.statusInfo);
-    model.typeInfo = new AdminResult().clone(model.typeInfo);
-    model.managerInfo = new AdminResult().clone(model.managerInfo);
-    model.parentInfo = new AdminResult().clone(model.parentInfo);
+    model.statusInfo && (model.statusInfo = AdminResult.createInstance(model.statusInfo));
+    model.managerInfo && (model.managerInfo = AdminResult.createInstance(model.managerInfo));
+    model.typeInfo && (model.typeInfo = AdminResult.createInstance(model.typeInfo));
+    model.parentInfo && (model.parentInfo = AdminResult.createInstance(model.parentInfo));
+    model.mawaredDepInfo && (model.mawaredDepInfo = AdminResult.createInstance(model.mawaredDepInfo));
 
     return model;
   }
