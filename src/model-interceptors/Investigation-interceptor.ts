d7a1d45bc78a5592d15af0456cf8cde7988e5966
aalshahin <aalshahin@eblacorp.com>
2023-11-01 15:57:33 +0400

    done init

--- src/model-interceptors/Investigation-interceptor.ts
+++ src/model-interceptors/Investigation-interceptor.ts
@@ -1,15 +1,19 @@
 import { ModelInterceptorContract } from 'cast-response';
 import { Investigation } from '@models/investigation';
+import { AdminResult } from '@models/admin-result';
 
 export class InvestigationInterceptor implements ModelInterceptorContract<Investigation> {
   send(model: Partial<Investigation>): Partial<Investigation> {
     delete model.$$__service_name__$$;
-    console.log('send')
     return model;
   }
 
   receive(model: Investigation): Investigation {
-    console.log('receive')
+    model.caseStatusInfo && (model.caseStatusInfo = AdminResult.createInstance(model.caseStatusInfo));
+    model.sectionInfo && (model.sectionInfo = AdminResult.createInstance(model.sectionInfo));
+    model.departmentInfo && (model.departmentInfo = AdminResult.createInstance(model.departmentInfo));
+    model.ouInfo && (model.ouInfo = AdminResult.createInstance(model.ouInfo));
+    model.creatorInfo && (model.creatorInfo = AdminResult.createInstance(model.creatorInfo));
     return model;
   }
 }
