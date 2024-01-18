4e6deb92cfccdd59ce6d8a5b51e187d5c8cd302c
BeSaRa <ahmedbesara@gmailcom>
2024-01-18 11:00:43 +0300

    apply prettier
    Signed-off-by: BeSaRa <ahmedbesara@gmailcom>

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
-const { compilerOptions } = require('./tsconfig');
-const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require("./tsconfig");
+const { pathsToModuleNameMapper } = require("ts-jest");
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'jest-preset-angular',
-  roots: ['<rootDir>/src/'],
-  testMatch: ['**/+(*.)+(spec).+(ts)'],
-  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+  preset: "jest-preset-angular",
+  roots: ["<rootDir>/src/"],
+  testMatch: ["**/+(*.)+(spec).+(ts)"],
+  setupFilesAfterEnv: ["<rootDir>/src/test.ts"],
   collectCoverage: true,
-  coverageReporters: ['text'],
-  coverageDirectory: 'coverage/my-app',
+  coverageReporters: ["text"],
+  coverageDirectory: "coverage/my-app",
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
-    prefix: '<rootDir>/',
+    prefix: "<rootDir>/",
   }),
 };
