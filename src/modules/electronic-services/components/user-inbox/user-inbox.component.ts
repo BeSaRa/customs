3559664b156d2f3b9314fed20322b5211311170b
aalshahin <aalshahin@eblacorp.com>
2024-01-28 16:41:34 +0400

    adding 25 option to inbox paginator and change the label text on it

--- src/modules/electronic-services/components/user-inbox/user-inbox.component.ts
+++ src/modules/electronic-services/components/user-inbox/user-inbox.component.ts
@@ -91,6 +91,15 @@ export class UserInboxComponent
         this.oldQueryResultSet = { ...value };
         this.dataSource.data = this.queryResultSet.items;
         this.dataSource.paginator = this.paginator;
+        this.paginator._intl.getRangeLabel = (
+          page: number,
+          pageSize: number,
+          length: number,
+        ) => {
+          const start = page * pageSize + 1;
+          const end = Math.min((page + 1) * pageSize, length);
+          return `${start} - ${end} ${this.lang.map.of} ${length}`;
+        };
       });
   }
 
