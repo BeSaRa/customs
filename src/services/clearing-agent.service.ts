6fbd0e7e6e0f7135f387db3407e3292afb2b087f
aalshahin <aalshahin@eblacorp.com>
2024-02-02 19:15:52 +0400

    adding integration popup component to clearing agent

--- src/services/clearing-agent.service.ts
+++ src/services/clearing-agent.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ClearingAgent } from '@models/clearing-agent';
-import { CastResponseContainer } from 'cast-response';
+import { CastResponse, CastResponseContainer } from 'cast-response';
 import { BaseCrudWithDialogService } from '@abstracts/base-crud-with-dialog-service';
 import { ComponentType } from '@angular/cdk/portal';
 import { ClearingAgentPopupComponent } from '@modules/administration/popups/clearing-agent-popup/clearing-agent-popup.component';
 import { Constructor } from '@app-types/constructors';
 import { Pagination } from '@models/pagination';
+import { Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
 
 @CastResponseContainer({
   $pagination: {
@@ -26,6 +28,7 @@ export class ClearingAgentService extends BaseCrudWithDialogService<
   ClearingAgent
 > {
   serviceName = 'ClearingAgentService';
+
   protected getModelClass(): Constructor<ClearingAgent> {
     return ClearingAgent;
   }
@@ -40,5 +43,20 @@ export class ClearingAgentService extends BaseCrudWithDialogService<
 
   getUrlSegment(): string {
     return this.urlService.URLS.CLEARING_AGENT;
+  }
+
+  @CastResponse()
+  syncForIntegration(options: {
+    startDate: string;
+    endDate: string;
+  }): Observable<ClearingAgent[]> {
+    return this.http.get<ClearingAgent[]>(
+      this.getUrlSegment() + `/integration`,
+      {
+        params: new HttpParams({
+          fromObject: { ...options },
+        }),
+      },
+    );
   }
 }
