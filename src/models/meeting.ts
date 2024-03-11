e39d1167809314632a863dede5c451d3903140b9
Abdulaziz5h <ite.abdo.sh.crafter.front@gmail.com>
2024-03-11 15:23:19 +0300

    add -- schedule meeting popup

--- src/models/meeting.ts
+++ src/models/meeting.ts
@@ -4,6 +4,7 @@ import { MeetingInterceptor } from '@model-interceptors/meeting-interceptor';
 import { MeetingService } from '@services/meeting.service';
 import { MeetingAttendance } from '@models/meeting-attendance';
 import { MeetingReport } from '@models/meeting-report';
+import { CustomValidators } from '@validators/custom-validators';
 
 const { send, receive } = new MeetingInterceptor();
 
@@ -15,9 +16,25 @@ export class Meeting extends BaseModel<Meeting, MeetingService> {
   title!: string;
   note!: string;
   meetingDate!: Date | string;
+  meetingTime!: string;
   createdBy!: number;
   createdOn!: Date | string;
   attendanceList: MeetingAttendance[] = [];
   reportList: MeetingReport[] = [];
   offenderList: number[] = [];
+
+  buildForm(controls: boolean = false) {
+    const { title, note, meetingDate, meetingTime } = this;
+    return {
+      title: controls ? [title, CustomValidators.required] : title,
+
+      note: controls ? [note, CustomValidators.required] : note,
+      meetingDate: controls
+        ? [meetingDate, CustomValidators.required]
+        : meetingDate,
+      meetingTime: controls
+        ? [meetingTime, CustomValidators.required]
+        : meetingTime,
+    };
+  }
 }
