4d49de9dde3668f0522041b5504735d2adc9fac4
Abdulaziz5h <ite.abdo.sh.crafter.front@gmail.com>
2024-03-11 15:23:19 +0300

    add -- schedule meeting popup

--- src/standalone/components/disciplinary-committee-meetings/disciplinary-committee-meetings.component.ts
+++ src/standalone/components/disciplinary-committee-meetings/disciplinary-committee-meetings.component.ts
@@ -4,13 +4,47 @@ import { LookupService } from '@services/lookup.service';
 import { Investigation } from '@models/investigation';
 import { DialogService } from '@services/dialog.service';
 import { MeetingService } from '@services/meeting.service';
-import { BehaviorSubject, switchMap } from 'rxjs';
+import { BehaviorSubject, Subject, switchMap } from 'rxjs';
 import { Meeting } from '@models/meeting';
+import { DatePipe } from '@angular/common';
+import { IconButtonComponent } from '@standalone/components/icon-button/icon-button.component';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatNoDataRow,
+  MatRow,
+  MatRowDef,
+  MatTable,
+} from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatTooltip } from '@angular/material/tooltip';
+import { ScheduleMeetingPopupComponent } from '@standalone/popups/schedule-meeting-popup/schedule-meeting-popup.component';
 
 @Component({
   selector: 'app-disciplinary-committee-meetings',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe,
+    IconButtonComponent,
+    MatCell,
+    MatCellDef,
+    MatColumnDef,
+    MatHeaderCell,
+    MatTable,
+    MatSort,
+    MatHeaderCellDef,
+    MatTooltip,
+    MatHeaderRow,
+    MatRowDef,
+    MatHeaderRowDef,
+    MatRow,
+    MatNoDataRow,
+  ],
   templateUrl: './disciplinary-committee-meetings.component.html',
   styleUrl: './disciplinary-committee-meetings.component.scss',
 })
@@ -21,9 +55,15 @@ export class DisciplinaryCommitteeMeetingsComponent implements OnInit {
   model = input.required<Investigation>();
   dialog = inject(DialogService);
   reload$: BehaviorSubject<null> = new BehaviorSubject<null>(null);
+  add$: Subject<void> = new Subject<void>();
+  view$: Subject<Meeting> = new Subject<Meeting>();
+  edit$: Subject<Meeting> = new Subject<Meeting>();
+  delete$: Subject<Meeting> = new Subject<Meeting>();
   dataList: Meeting[] = [];
+  displayedColumns: string[] = ['actions'];
   ngOnInit(): void {
     this._listenToReload();
+    this._listenToAddMeeting();
   }
   _listenToReload() {
     this.reload$
@@ -37,5 +77,21 @@ export class DisciplinaryCommitteeMeetingsComponent implements OnInit {
       .subscribe(res => {
         this.dataList = res.rs;
       });
+  }
+
+  _listenToAddMeeting() {
+    this.add$
+      .pipe(
+        switchMap(() => {
+          return this.dialog
+            .open(ScheduleMeetingPopupComponent, {
+              data: {
+                model: new Meeting(),
+              },
+            })
+            .afterClosed();
+        }),
+      )
+      .subscribe(() => {});
   }
 }
---
+++ src/standalone/popups/schedule-meeting-popup/schedule-meeting-popup.component.ts
@@ -0,0 +1,66 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { IconButtonComponent } from '@standalone/components/icon-button/icon-button.component';
+import { MAT_DIALOG_DATA, MatDialogClose } from '@angular/material/dialog';
+import { ButtonComponent } from '@standalone/components/button/button.component';
+import { Observable } from 'rxjs';
+import { TextareaComponent } from '@standalone/components/textarea/textarea.component';
+import { InputComponent } from '@standalone/components/input/input.component';
+import { ReactiveFormsModule, UntypedFormGroup } from '@angular/forms';
+import {
+  MatDatepicker,
+  MatDatepickerInput,
+} from '@angular/material/datepicker';
+import { Meeting } from '@models/meeting';
+import { AdminDialogComponent } from '@abstracts/admin-dialog-component';
+import { CrudDialogDataContract } from '@contracts/crud-dialog-data-contract';
+
+import { ControlDirective } from '@standalone/directives/control.directive';
+import {
+  NgxMatTimepickerComponent,
+  NgxMatTimepickerDirective,
+} from 'ngx-mat-timepicker';
+
+@Component({
+  selector: 'app-schedule-meeting-popup',
+  standalone: true,
+  imports: [
+    IconButtonComponent,
+    MatDialogClose,
+    ButtonComponent,
+    TextareaComponent,
+    InputComponent,
+    ReactiveFormsModule,
+    MatDatepickerInput,
+    MatDatepicker,
+    ControlDirective,
+    NgxMatTimepickerComponent,
+    NgxMatTimepickerDirective,
+  ],
+  templateUrl: './schedule-meeting-popup.component.html',
+  styleUrl: './schedule-meeting-popup.component.scss',
+})
+export class ScheduleMeetingPopupComponent
+  extends AdminDialogComponent<Meeting>
+  implements OnInit
+{
+  override data: CrudDialogDataContract<Meeting, { [index: string]: unknown }> =
+    inject(MAT_DIALOG_DATA);
+  todayDate = new Date();
+  form!: UntypedFormGroup;
+  protected override _beforeSave(): boolean | Observable<boolean> {
+    return this.form.valid;
+  }
+  protected override _prepareModel(): Meeting | Observable<Meeting> {
+    const formValue = this.form.getRawValue();
+    return new Meeting().clone<Meeting>({
+      ...formValue,
+    });
+  }
+  protected override _afterSave(model: Meeting): void {
+    this.dialogRef.close(model);
+  }
+
+  _buildForm() {
+    this.form = this.fb.group(this.model.buildForm(true));
+  }
+}
