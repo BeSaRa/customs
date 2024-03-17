04e5b5eb760f3a821183c483865b0fc7ca554fcf
BeSaRa <ahmedbesara@gmailcom>
2024-03-17 15:34:05 +0300

    "revert" fix: person id
    Signed-off-by: BeSaRa <ahmedbesara@gmailcom>

--- src/standalone/popups/call-request-popup/call-request-popup.component.ts
+++ src/standalone/popups/call-request-popup/call-request-popup.component.ts
@@ -74,9 +74,7 @@ export class CallRequestPopupComponent extends AdminDialogComponent<CallRequest>
     this.isOffender() ? SummonType.OFFENDER : SummonType.WITNESS,
   );
   personId = computed(() => {
-    return this.isOffender()
-      ? this.offender()!.offenderRefId
-      : this.witness()!.witnessRefId;
+    return this.isOffender() ? this.offender()!.id : this.witness()!.id;
   });
 
   person = computed(() => {
--- src/standalone/popups/investigation-report-poup/investigation-report-popup.component.ts
+++ src/standalone/popups/investigation-report-poup/investigation-report-popup.component.ts
@@ -92,9 +92,7 @@ export class InvestigationReportPopupComponent extends AdminDialogComponent<Inve
     this.isOffender() ? SummonType.OFFENDER : SummonType.WITNESS,
   );
   personId = computed(() => {
-    return this.isOffender()
-      ? this.offender()!.offenderRefId
-      : this.witness()!.witnessRefId;
+    return this.isOffender() ? this.offender()!.id : this.witness()!.id;
   });
 
   config = inject(ConfigService);
