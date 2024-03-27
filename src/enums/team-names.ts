9fd54e226beb669a4d0b771805ad0171beb99036
BeSaRa <ahmedbesara@gmailcom>
2024-03-27 14:04:47 +0300

    create team names
    Signed-off-by: BeSaRa <ahmedbesara@gmailcom>

--- src/enums/team-names.ts
+++ src/enums/team-names.ts
@@ -1,42 +1,42 @@
 export enum TeamNames {
-  //  الادارة المقدمة
+  /** الادارة المقدمة **/
   Applicant_Department = 'Applicant Department',
-  // رئيس قسم الادارة المقدمة
+  /**رئيس قسم الادارة المقدمة **/
   Applicant_Department_Chief = 'Applicant Department Chief',
-  // مدير الادارة المقدمة
+  /** مدير الادارة المقدمة **/
   Applicant_Department_Manager = 'Applicant Department Manager',
-  //  الموارد البشرية
+  /** الموارد البشرية **/
   Human_Resources = 'Human Resources',
-  // مدير الموارد البشرية
+  /** مدير الموارد البشرية **/
   Human_Resources_Manager = 'Human Resources Manager',
-  //  الشؤون الجمركية
+  /** الشؤون الجمركية **/
   Customs_Affairs = 'Customs_Affairs',
-  // مدير الشؤون الجمركية
+  /** مدير الشؤون الجمركية **/
   Customs_Affairs_Manager = 'Customs Affairs Manager',
-  // الرئيس
+  /** الرئيس **/
   President = 'President',
-  // مكتب الرئيس
+  /** مكتب الرئيس **/
   President_Office = 'President Office',
-  //  مساعد الرئيس
+  /** مساعد الرئيس **/
   President_Assistant = 'President Assistant',
-  //مكتب مساعد الرئيس
+  /** مكتب مساعد الرئيس **/
   President_Assistant_Office = 'President Assistant Office',
-  // اللجنة التأديبية
+  /** اللجنة التأديبية **/
   Disciplinary_Committee = 'Disciplinary Committee',
-  // المجلس الدائم للتأديب
+  /** المجلس الدائم للتأديب **/
   Permanent_Disciplinary_Committee = 'Permanent Disciplinary Committee',
-  // الشؤون القانونية
+  /** الشؤون القانونية **/
   Legal_Affairs = 'Legal Affairs',
-  //مدير الشؤون القانونية
+  /** مدير الشؤون القانونية **/
   Legal_Affairs_Manager = 'Legal Affairs Manager',
-  //مساعد رئيس الشؤون الجمركيه
+  /** مساعد رئيس الشؤون الجمركيه **/
   CA_President_Assistant = 'CA President Assistant',
-  //مكتب مساعد رئيس الشؤون الجمركيه
+  /** مكتب مساعد رئيس الشؤون الجمركيه **/
   CA_President_Assistant_Office = 'CA President Assistant Office',
-  //المحققين
+  /** المحققين **/
   Investigator = 'Investigator',
-  //رئيس مكتب المحققين
+  /** رئيس مكتب المحققين **/
   Investigation_Chief = 'Investigation Chief',
-  // مدير نظام الجمارك
+  /** مدير نظام الجمارك **/
   CDI_Admins = 'CDI Admins',
 }
