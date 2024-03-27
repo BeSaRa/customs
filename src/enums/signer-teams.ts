298ce057e106421c4945016fe90449032022cdf7
BeSaRa <ahmedbesara@gmailcom>
2024-03-27 14:04:47 +0300

    create team names
    Signed-off-by: BeSaRa <ahmedbesara@gmailcom>

--- src/enums/signer-teams.ts
+++
@@ -1,7 +0,0 @@
-export enum SignerTeams {
-  MANAGER_DIRECTOR = 'Applicant Department Manager',
-  PRESIDENT_ASSISTANT = 'President Assistant',
-  PRESIDENT_ASSISTANT_CUSTOMS_AFFAIR = 'President Assistant Customs Affair',
-  PERMANENT_DISCIPLINARY_COUNCIL = 'PERMANENT_DISCIPLINARY_COUNCIL',
-  DISCIPLINARY_COUNCIL = 'Disciplinary Council',
-}
---
+++ src/enums/team-names.ts
@@ -0,0 +1,42 @@
+export enum TeamNames {
+  //  الادارة المقدمة
+  Applicant_Department = 'Applicant Department',
+  // رئيس قسم الادارة المقدمة
+  Applicant_Department_Chief = 'Applicant Department Chief',
+  // مدير الادارة المقدمة
+  Applicant_Department_Manager = 'Applicant Department Manager',
+  //  الموارد البشرية
+  Human_Resources = 'Human Resources',
+  // مدير الموارد البشرية
+  Human_Resources_Manager = 'Human Resources Manager',
+  //  الشؤون الجمركية
+  Customs_Affairs = 'Customs_Affairs',
+  // مدير الشؤون الجمركية
+  Customs_Affairs_Manager = 'Customs Affairs Manager',
+  // الرئيس
+  President = 'President',
+  // مكتب الرئيس
+  President_Office = 'President Office',
+  //  مساعد الرئيس
+  President_Assistant = 'President Assistant',
+  //مكتب مساعد الرئيس
+  President_Assistant_Office = 'President Assistant Office',
+  // اللجنة التأديبية
+  Disciplinary_Committee = 'Disciplinary Committee',
+  // المجلس الدائم للتأديب
+  Permanent_Disciplinary_Committee = 'Permanent Disciplinary Committee',
+  // الشؤون القانونية
+  Legal_Affairs = 'Legal Affairs',
+  //مدير الشؤون القانونية
+  Legal_Affairs_Manager = 'Legal Affairs Manager',
+  //مساعد رئيس الشؤون الجمركيه
+  CA_President_Assistant = 'CA President Assistant',
+  //مكتب مساعد رئيس الشؤون الجمركيه
+  CA_President_Assistant_Office = 'CA President Assistant Office',
+  //المحققين
+  Investigator = 'Investigator',
+  //رئيس مكتب المحققين
+  Investigation_Chief = 'Investigation Chief',
+  // مدير نظام الجمارك
+  CDI_Admins = 'CDI Admins',
+}
