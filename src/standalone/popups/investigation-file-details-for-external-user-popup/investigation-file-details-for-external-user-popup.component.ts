c3199478a8713e3fe4c923d9b22a7e035402940f
Abdulaziz5h <ite.abdo.sh.crafter.front@gmail.com>
2024-06-24 19:42:51 +0300

    Bug 62835: External portal - employee - In investigation files when open the more details, only the violations related to this offender in this request should be shown

--- src/standalone/popups/investigation-file-details-for-external-user-popup/investigation-file-details-for-external-user-popup.component.ts
+++ src/standalone/popups/investigation-file-details-for-external-user-popup/investigation-file-details-for-external-user-popup.component.ts
@@ -10,6 +10,8 @@ import { LangService } from '@services/lang.service';
 import { Config } from '@constants/config';
 import { DatePipe } from '@angular/common';
 import { OffenderViolationsComponent } from '@standalone/components/offender-violations/offender-violations.component';
+import { EmployeeService } from '@services/employee.service';
+import { OffenderViolation } from '@models/offender-violation';
 
 @Component({
   selector: 'app-investigation-file-details-for-external-user-popup',
@@ -33,6 +35,13 @@ import { OffenderViolationsComponent } from '@standalone/components/offender-vio
 export class InvestigationFileDetailsForExternalUserPopupComponent {
   lang = inject(LangService);
   data = inject(MAT_DIALOG_DATA);
+  loggedInUserId = inject(EmployeeService).loggedInUserId;
   model = this.data.model;
   protected readonly Config = Config;
+  get offenderViolationInfo() {
+    return this.model.offenderViolationInfo.filter(
+      (offenderViolation: OffenderViolation) =>
+        offenderViolation.offenderRefId === this.loggedInUserId,
+    );
+  }
 }
