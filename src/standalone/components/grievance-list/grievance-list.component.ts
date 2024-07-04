ab4bfed883691da4558ca7d0b43da35a14466243
Abdulaziz5h <ite.abdo.sh.crafter.front@gmail.com>
2024-07-04 14:26:01 +0300

    Bug 63127: External portal - Grievances - decision number didn't display in applied Grievances tab

--- src/standalone/components/grievance-list/grievance-list.component.ts
+++ src/standalone/components/grievance-list/grievance-list.component.ts
@@ -79,6 +79,7 @@ export class GrievanceListComponent
 
   dataSource: MatTableDataSource<Grievance> = new MatTableDataSource();
   grievanceDisplayedColumns = [
+    'grievanceFullSerial',
     'caseIdentifier',
     'investigationFullSerial',
     'caseStatus',
