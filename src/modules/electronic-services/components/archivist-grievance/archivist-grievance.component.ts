2cf2c32a5d025429cbde3c533ed544690a486bba
Ameer-Shahoud <ameer.shahoud.1997@gmail.com>
2024-07-14 16:03:53 +0300

    fix Bug 63315

--- src/modules/electronic-services/components/archivist-grievance/archivist-grievance.component.ts
+++ src/modules/electronic-services/components/archivist-grievance/archivist-grievance.component.ts
@@ -1,17 +1,16 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { LangService } from '@services/lang.service';
-import { DialogService } from '@services/dialog.service';
-import { LookupService } from '@services/lookup.service';
 import {
   ReactiveFormsModule,
   UntypedFormBuilder,
   UntypedFormGroup,
 } from '@angular/forms';
-import { ButtonComponent } from '@standalone/components/button/button.component';
-import { ControlDirective } from '@standalone/directives/control.directive';
-import { IconButtonComponent } from '@standalone/components/icon-button/icon-button.component';
-import { InputComponent } from '@standalone/components/input/input.component';
 import { MatCard } from '@angular/material/card';
+import {
+  MatDatepicker,
+  MatDatepickerInput,
+} from '@angular/material/datepicker';
+import { MatSort } from '@angular/material/sort';
 import {
   MatCell,
   MatCellDef,
@@ -26,16 +25,33 @@ import {
   MatTable,
   MatTableDataSource,
 } from '@angular/material/table';
-import { NgTemplateOutlet } from '@angular/common';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
-import { MatSort } from '@angular/material/sort';
 import { MatTooltip } from '@angular/material/tooltip';
-import { SelectInputComponent } from '@standalone/components/select-input/select-input.component';
-import {
-  MatDatepicker,
-  MatDatepickerInput,
-} from '@angular/material/datepicker';
 import { ActivatedRoute, Router } from '@angular/router';
+import { OffenderStatusEnum } from '@enums/offender-status.enum';
+import { ReportStatus } from '@enums/report-status';
+import { UserClick } from '@enums/user-click';
+import { ColumnsWrapper } from '@models/columns-wrapper';
+import { NoneFilterColumn } from '@models/none-filter-column';
+import { Offender } from '@models/offender';
+import { Pagination } from '@models/pagination';
+import { Penalty } from '@models/penalty';
+import { PenaltyDecision } from '@models/penalty-decision';
+import { PenaltyDecisionCriteria } from '@models/penalty-decision-criteria';
+import { TextFilterColumn } from '@models/text-filter-column';
+import { DialogService } from '@services/dialog.service';
+import { LangService } from '@services/lang.service';
+import { LookupService } from '@services/lookup.service';
+import { OffenderService } from '@services/offender.service';
+import { PenaltyService } from '@services/penalty.service';
+import { ButtonComponent } from '@standalone/components/button/button.component';
+import { GrievanceListComponent } from '@standalone/components/grievance-list/grievance-list.component';
+import { IconButtonComponent } from '@standalone/components/icon-button/icon-button.component';
+import { InputComponent } from '@standalone/components/input/input.component';
+import { SelectInputComponent } from '@standalone/components/select-input/select-input.component';
+import { ControlDirective } from '@standalone/directives/control.directive';
+import { GrievancePopupComponent } from '@standalone/popups/grievance-popup/grievance-popup.component';
+import { ignoreErrors } from '@utils/utils';
 import {
   catchError,
   exhaustMap,
@@ -47,21 +63,6 @@ import {
   switchMap,
   throwError,
 } from 'rxjs';
-import { ColumnsWrapper } from '@models/columns-wrapper';
-import { NoneFilterColumn } from '@models/none-filter-column';
-import { ignoreErrors } from '@utils/utils';
-import { PenaltyDecision } from '@models/penalty-decision';
-import { Pagination } from '@models/pagination';
-import { PenaltyDecisionCriteria } from '@models/penalty-decision-criteria';
-import { TextFilterColumn } from '@models/text-filter-column';
-import { GrievancePopupComponent } from '@standalone/popups/grievance-popup/grievance-popup.component';
-import { UserClick } from '@enums/user-click';
-import { GrievanceListComponent } from '@standalone/components/grievance-list/grievance-list.component';
-import { OffenderService } from '@services/offender.service';
-import { Offender } from '@models/offender';
-import { Penalty } from '@models/penalty';
-import { PenaltyService } from '@services/penalty.service';
-import { ReportStatus } from '@enums/report-status';
 
 @Component({
   selector: 'app-archivist-grievance',
@@ -115,6 +116,8 @@ export class ArchivistGrievanceComponent implements OnInit {
   displayedList = new MatTableDataSource<Offender>();
   selectedTabIndex = 0;
   grievance$: Subject<PenaltyDecision> = new Subject<PenaltyDecision>();
+
+  readonly OffenderStatusEnum = OffenderStatusEnum;
 
   ngOnInit(): void {
     this.form = this.fb.group(new PenaltyDecisionCriteria().buildForm());
