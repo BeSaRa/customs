376bc7901c303a1abc552d355ab403bd40863dff
aalshahin <aalshahin@eblacorp.com>
2024-08-13 15:35:49 +0400

    working on menu items

--- src/modules/administration/components/custom-menu-url-handler/custom-menu-url-handler.component.ts
+++ src/modules/administration/components/custom-menu-url-handler/custom-menu-url-handler.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { inject } from '@angular/core';
 import { LangService } from '@services/lang.service';
 import {
@@ -9,18 +9,14 @@ import {
   UntypedFormControl,
 } from '@angular/forms';
 import { LookupService } from '@services/lookup.service';
-import { DialogService } from '@services/dialog.service';
-import { CustomMenuService } from '@services/custom-menu.service';
 import { CustomMenu } from '@models/custom-menu';
 import { Lookup } from '@models/lookup';
 import { MenuUrlValueContract } from '@contracts/menu-url-value-contract';
 import {
   CdkDrag,
   CdkDragDrop,
-  CdkDragEnter,
-  CdkDragExit,
+  CdkDragHandle,
   CdkDropList,
-  copyArrayItem,
 } from '@angular/cdk/drag-drop';
 import { TextareaComponent } from '@standalone/components/textarea/textarea.component';
 import { AsyncPipe } from '@angular/common';
@@ -38,10 +34,17 @@ import {
   MatRow,
   MatRowDef,
   MatTable,
+  MatTableModule,
 } from '@angular/material/table';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { MatSort, MatSortHeader } from '@angular/material/sort';
+import { CdkCell } from '@angular/cdk/table';
+import { MatTooltip } from '@angular/material/tooltip';
+import { CustomMenuService } from '@services/custom-menu.service';
+import { ButtonComponent } from '@standalone/components/button/button.component';
+import { generateHtmlList, isValidValue } from '@utils/utils';
+import { DialogService } from '@services/dialog.service';
 
 @Component({
   selector: 'app-custom-menu-url-handler',
@@ -68,11 +71,18 @@ import { MatSort, MatSortHeader } from '@angular/material/sort';
     MatSort,
     MatSortHeader,
     MatTable,
+    MatTableModule,
+    CdkDropList,
+    CdkDrag,
+    CdkDragHandle,
+    CdkCell,
+    MatTooltip,
+    ButtonComponent,
   ],
   templateUrl: './custom-menu-url-handler.component.html',
   styleUrl: './custom-menu-url-handler.component.scss',
 })
-export class CustomMenuUrlHandlerComponent implements OnInit {
+export class CustomMenuUrlHandlerComponent implements OnInit, AfterViewInit {
   @Input() record!: CustomMenu;
   @Input() readonly: boolean = false;
 
@@ -80,8 +90,8 @@ export class CustomMenuUrlHandlerComponent implements OnInit {
   lang = inject(LangService);
   fb = inject(FormBuilder);
   lookupService = inject(LookupService);
-  dialogService = inject(DialogService);
   customMenuService = inject(CustomMenuService);
+  dialog = inject(DialogService);
 
   ngOnInit(): void {
     this._buildForm();
@@ -97,46 +107,36 @@ export class CustomMenuUrlHandlerComponent implements OnInit {
     this.checkUrlVariables();
   }
 
-  private _buildForm(): any {
-    this.form = this.fb.group(this.record.buildMenuUrlForm(true));
-    // this.readonly
-    //   ? this.menuUrlControl.disable()
-    //   : this.menuUrlControl.enable();
+  checkUrlVariables(userInteraction: boolean = false): void {
+    const newVariableList: string[] = this.customMenuService.findVariablesInUrl(
+      this.menuUrlControl.value,
+    );
+    if (userInteraction && !newVariableList.length) {
+      this.dialog.warning(this.lang.map.no_variable_found);
+      return;
+    }
+    const duplicateVariablesList = this._getDuplicateVariables(newVariableList);
+    if (duplicateVariablesList.length) {
+      const listHtml = generateHtmlList(
+        this.lang.map.duplicate_url_variables,
+        duplicateVariablesList,
+      );
+      this.dialog.error(listHtml.outerHTML);
+      console.log('Duplicate VariablesL: ', duplicateVariablesList);
+      return;
+    }
 
-    this.initUrlHandler();
-  }
-
-  // isValidUrl(): boolean {
-  //   const url = this.menuUrlControl.value;
-  //   if (this.record.isParentMenu()) {
-  //     // if no url, its valid
-  //     if (!CommonUtils.isValidValue(url)) {
-  //       return true;
-  //     }
-  //   } else {
-  //     // children must have url
-  //     if (!CommonUtils.isValidValue(url)) {
-  //       return false;
-  //     }
-  //   }
-  //   // menu should have valid url and valid variables too
-  //   const variables = this.customMenuService.findVariablesInUrl(url);
-  //   return (
-  //     this.menuUrlControl.valid &&
-  //     (!variables.length || this.isValidVariableList)
-  //   );
-  // }
-
-  isTouchedOrDirty(): boolean {
-    return this.form && (this.form.touched || this.form.dirty);
-  }
-
-  get isValidVariableList(): boolean {
-    return this.variableList.every(item => item.valueLookups.length > 0);
-  }
-
-  get menuUrlControl(): UntypedFormControl {
-    return this.form.get('menuURL') as UntypedFormControl;
+    const existingVariableList = [...this.variableList];
+    this.variableList = newVariableList.map((newVariable: string) => {
+      const exisingVariable = existingVariableList.find(
+        x => x.name.trim().toLowerCase() === newVariable.trim().toLowerCase(),
+      );
+      return <MenuUrlValueContract>{
+        name: newVariable,
+        value: exisingVariable ? exisingVariable.value : undefined,
+        valueLookups: exisingVariable ? exisingVariable.valueLookups : [],
+      };
+    });
   }
 
   private _getDuplicateVariables(variableList: string[]): string[] {
@@ -147,77 +147,53 @@ export class CustomMenuUrlHandlerComponent implements OnInit {
       });
   }
 
-  checkUrlVariables(userInteraction: boolean = false): void {
-    // const newVariableList: string[] = this.customMenuService.findVariablesInUrl(
-    //   this.menuUrlControl.value,
-    // );
-    // if (userInteraction && !newVariableList.length) {
-    //   this.toast.info(this.lang.map.msg_no_variables_found);
-    //   return;
-    // }
-    // const duplicateVariablesList = this._getDuplicateVariables(newVariableList);
-    // if (duplicateVariablesList.length) {
-    //   const listHtml = CommonUtils.generateHtmlList(
-    //     this.lang.map.msg_duplicate_url_variables,
-    //     duplicateVariablesList,
-    //   );
-    //   this.dialogService.error(listHtml.outerHTML);
-    //   return;
-    // }
-    //
-    // const existingVariableList = [...this.variableList];
-    // this.variableList = newVariableList.map((newVariable: string) => {
-    //   const exisingVariable = existingVariableList.find(
-    //     x => x.name.trim().toLowerCase() === newVariable.trim().toLowerCase(),
-    //   );
-    //   return <MenuUrlValueContract>{
-    //     name: newVariable,
-    //     value: exisingVariable ? exisingVariable.value : undefined,
-    //     valueLookups: exisingVariable ? exisingVariable.valueLookups : [],
-    //   };
-    // });
+  private _buildForm() {
+    this.form = this.fb.group(this.record.buildMenuUrlForm(true));
   }
 
-  getConnectedListIds(): string[] {
-    return this.variableList.map(
-      (item, index) => this.dropListIdInitials + index,
-    );
+  ngAfterViewInit(): void {
+    this.readonly
+      ? this.menuUrlControl.disable()
+      : this.menuUrlControl.enable();
+
+    this.initUrlHandler();
   }
 
-  canDropValue(item: CdkDrag<Lookup>, dropList: CdkDropList): boolean {
-    return !dropList.data.length;
-  }
-
-  onExitSourceList(event: CdkDragExit) {
-    const currentIdx = event.container.data.findIndex(
-      (item: Lookup) => item.lookupKey === event.item.data.lookupKey,
-    );
-
-    const replacementValue = new Lookup().clone({
-      ...event.item.data,
-      temp: true,
-    });
-    this._addDummyItemToSourceList(
-      currentIdx,
-      replacementValue as unknown as Lookup,
-    );
-  }
-
-  onEnterSourceList(event: CdkDragEnter) {
-    this._filterSourceList();
-  }
-
-  drop(event: CdkDragDrop<Lookup[]>) {
-    if (event.previousContainer !== event.container) {
-      copyArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
+  isValidUrl(): boolean {
+    const url = this.menuUrlControl.value;
+    if (this.record.isParentMenu()) {
+      // if no url, its valid
+      if (!isValidValue(url)) {
+        return true;
+      }
+    } else {
+      // children must have url
+      if (!isValidValue(url)) {
+        return false;
+      }
     }
-    if (event.previousContainer.data) {
-      this._filterSourceList();
+    // menu should have valid url and valid variables too
+    const variables = this.customMenuService.findVariablesInUrl(url);
+    return (
+      this.menuUrlControl.valid &&
+      (!variables.length || this.isValidVariableList)
+    );
+  }
+
+  get isValidVariableList(): boolean {
+    return this.variableList.every(item => item.valueLookups.length > 0);
+  }
+
+  get menuUrlControl(): UntypedFormControl {
+    return this.form.get('menuURL') as UntypedFormControl;
+  }
+
+  drop(event: CdkDragDrop<Lookup[]>, row: MenuUrlValueContract): void {
+    if (event.previousContainer !== event.container) {
+      row.valueLookups = [];
+
+      const item = event.previousContainer.data[event.previousIndex];
+      row.valueLookups.push(item);
     }
   }
 
@@ -226,26 +202,22 @@ export class CustomMenuUrlHandlerComponent implements OnInit {
   }
 
   private _filterSourceList() {
-    // this.menuParamsList = this.menuParamsList.filter(
-    //   (item: Lookup) => !item.temp,
-    // );
+    this.menuParamsList = this.menuParamsList.filter(
+      (item: Lookup) => !item.temp,
+    );
   }
 
   removeVariableValue(item: MenuUrlValueContract): void {
     if (!item.valueLookups || item.valueLookups.length === 0) {
       return;
     }
-    // this.dialogService
-    //   .confirm(
-    //     this.lang.map.msg_confirm_delete_x.change({
-    //       x: item.valueLookups[0].getName(),
-    //     }),
-    //   )
-    //   .onAfterClose$.subscribe((clickOn: UserClickOn) => {
-    //     if (clickOn === UserClickOn.YES) {
-    //       item.value = undefined;
-    //       item.valueLookups = [];
-    //     }
-    //   });
+    item.value = undefined;
+    item.valueLookups = [];
   }
+
+  getConnectedListIds(): string[] {
+    return this.variableList.map((_, index) => `dropList-${index}`);
+  }
+
+  protected readonly Lookup = Lookup;
 }
