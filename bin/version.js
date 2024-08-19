5a90e3334fac71b8bfad6190aa0b7046e25c8d81
BeSaRa <ahmedbesara@gmailcom>
2024-08-19 13:04:09 +0300

    remove package lock
    Signed-off-by: BeSaRa <ahmedbesara@gmailcom>

--- bin/version.js
+++ bin/version.js
@@ -25,16 +25,13 @@ if (!vType) {
 
 const configPath = "src/constants/config.ts";
 const pkgPath = "package.json";
-const pkgLockPath = "package-lock.json";
 const pkg = JSON.parse(readFileSync(pkgPath, "utf8"));
-const pkgLock = JSON.parse(readFileSync(pkgLockPath, "utf8"));
 let configContent = readFileSync(configPath, "utf-8");
 const [major, minor, patch] = pkg.version.split(".");
 const newVersion = correctVersion(major, minor, patch, vType);
 const appVersion = "v" + newVersion;
 
 pkg.version = newVersion;
-pkgLock.version = newVersion;
 
 configContent = configContent.replace(
   /[^_]VERSION:.*,$/gm,
@@ -42,14 +39,13 @@ configContent = configContent.replace(
 );
 
 writeFileSync(pkgPath, JSON.stringify(pkg, null, "  "), "utf-8");
-writeFileSync(pkgLockPath, JSON.stringify(pkgLock, null, "  "), "utf-8");
+
 writeFileSync(configPath, configContent, "utf-8");
-setTimeout(() => {
-  exec("git add .", function (error) {
-    if (error) console.log(error);
-    exec(`git commit -m ${newVersion}`, function () {
-      exec(`git tag -a ${appVersion} -m ${newVersion}`);
-      console.log(`Version Created: ${appVersion}`);
-    });
+
+exec("git add .", function (error) {
+  if (error) console.log(error);
+  exec(`git commit -m ${newVersion}`, function () {
+    exec(`git tag -a ${appVersion} -m ${newVersion}`);
+    console.log(`Version Created: ${appVersion}`);
   });
-}, 2000);
+});
