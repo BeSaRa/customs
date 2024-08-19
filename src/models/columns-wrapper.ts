004e55f3424ac235644aeb348cee59e0299fd307
aalshahin <aalshahin@eblacorp.com>
2024-08-19 12:59:40 +0400

    Revert "making internal user page show username depending on current lang"
    This reverts commit 55a805c29638ddce264130b281fceaa8e82c2123.

--- src/models/columns-wrapper.ts
+++ src/models/columns-wrapper.ts
@@ -1,7 +1,6 @@
 import { FilterColumnContract } from '@contracts/filter-column-contract';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { ColumnMapContract } from '@contracts/column-map-contract';
-import { LangCodes } from '@enums/lang-codes';
 
 export class ColumnsWrapper<M> {
   private readonly list: FilterColumnContract[] = [];
@@ -13,8 +12,6 @@ export class ColumnsWrapper<M> {
     ColumnMapContract<M>
   >;
   private filter$!: BehaviorSubject<Partial<M>>;
-  private langCode$!: BehaviorSubject<LangCodes>;
-
   constructor(...args: FilterColumnContract[]) {
     this.list = args;
     this.list.forEach(colum => {
@@ -30,36 +27,5 @@ export class ColumnsWrapper<M> {
   attacheFilter(filter: BehaviorSubject<Partial<M>>): this {
     this.filter$ = filter;
     return this;
-  }
-
-  setLangCode(langCode$: BehaviorSubject<LangCodes>): this {
-    this.langCode$ = langCode$;
-    this.langCode$.subscribe(code => {
-      this.updateColumns(code);
-    });
-    return this;
-  }
-
-  private updateColumns(langCode?: LangCodes): void {
-    const langColumnName = langCode + 'Name';
-    this.displayedColumns = [];
-    this.filteredColumns = [];
-
-    this.list.forEach(column => {
-      const shouldDisplay =
-        column.name === langColumnName ||
-        !['arName', 'enName'].includes(column.name);
-
-      if (shouldDisplay) {
-        this.displayedColumns.push(column.name);
-        this.filteredColumns.push(column.filterName);
-
-        this.columnsMap[column.filterName] = {
-          config: column,
-          filter: () => this.filter$,
-          clear: this.clear$,
-        } as ColumnMapContract<M>;
-      }
-    });
   }
 }
