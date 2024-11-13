1593a4490ec7e7d18848d6667fcc06d6ff52bccd
Ameer-Shahoud <ameer.shahoud.1997@gmail.com>
2024-11-13 12:49:33 +0300

    fix Bug 62706: LA - in LA manager last step, when the request has offenders (employees and brokers) the investigation results added for the employees is replaced by the one added for the brokers

--- src/standalone/components/legal-affairs-procedures/legal-affairs-procedures.component.ts
+++ src/standalone/components/legal-affairs-procedures/legal-affairs-procedures.component.ts
@@ -6,11 +6,10 @@ import {
   Output,
   viewChild,
 } from '@angular/core';
-import { PersonsListComponent } from '@standalone/components/legal-affairs-offenders/persons-list.component';
 import { Investigation } from '@models/investigation';
-import { MemorandumOpinionListComponent } from '@standalone/components/memorandum-opinion-list/memorandum-opinion-list.component';
 import { EmployeeService } from '@services/employee.service';
-import { Memorandum } from '@models/memorandum';
+import { PersonsListComponent } from '@standalone/components/legal-affairs-offenders/persons-list.component';
+import { MemorandumOpinionListComponent } from '@standalone/components/memorandum-opinion-list/memorandum-opinion-list.component';
 
 @Component({
   selector: 'app-legal-affairs-procedures',
@@ -33,28 +32,5 @@ export class LegalAffairsProceduresComponent {
       'VIEW_WITNESS',
       'MANAGE_WITNESS',
     ]);
-  }
-
-  getLastApprovedLegalMemo() {
-    const memos = this.memorandumMasterComponent()
-      ?.models as unknown as Memorandum[];
-    return this.getLastModifiedApprovedMemo(memos);
-  }
-
-  getLastModifiedApprovedMemo(memos: Memorandum[]) {
-    let lastModifiedApprovedMemo: Memorandum = memos[0];
-
-    memos.forEach(memo => {
-      if (memo.isApproved) {
-        if (
-          !lastModifiedApprovedMemo ||
-          new Date(memo.lastModified) >
-            new Date(lastModifiedApprovedMemo.lastModified)
-        ) {
-          lastModifiedApprovedMemo = memo;
-        }
-      }
-    });
-    return lastModifiedApprovedMemo;
   }
 }
