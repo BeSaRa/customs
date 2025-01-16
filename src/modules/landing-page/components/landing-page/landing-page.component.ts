98d3c8c18bb631250f44def6c9cbe45fa70f1f07
Ameer-Shahoud <ameer.shahoud.1997@gmail.com>
2025-01-16 12:09:28 +0300

    fix Bug 64870: Dashboard - enhancement - need to add refresh icon to reload all counters and charts

--- src/modules/landing-page/components/landing-page/landing-page.component.ts
+++ src/modules/landing-page/components/landing-page/landing-page.component.ts
@@ -1,14 +1,17 @@
-import { Component, inject, OnDestroy } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { InboxCounterService } from '@services/inbox-counter.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
-  providers: [InboxCounterService],
 })
-export class LandingPageComponent implements OnDestroy {
+export class LandingPageComponent implements OnInit, OnDestroy {
   private _inboxCounterService = inject(InboxCounterService);
+
+  ngOnInit(): void {
+    this._inboxCounterService.startPolling();
+  }
 
   ngOnDestroy(): void {
     this._inboxCounterService.stopPolling();
