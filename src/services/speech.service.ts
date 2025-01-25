aedb2bb12e41ad18015bff5fbc0d8103451b8d36
aalshahin <aalshahin@eblacorp.com>
2025-01-25 17:01:12 +0400

    removing the error dialog from the generate speech token

--- src/services/speech.service.ts
+++ src/services/speech.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { SpeechTokenContract } from '@constants/speech-token-contract';
 import { UrlService } from '@services/url.service';
 import { AppStore } from '@stores/app.store';
-import { tap } from 'rxjs';
+import { catchError, EMPTY, tap } from 'rxjs';
+import { NO_ERROR_HANDLE, NO_LOADER_TOKEN } from '@http-contexts/tokens';
 
 @Injectable({
   providedIn: 'root',
@@ -18,9 +19,13 @@ export class SpeechService {
   }
 
   generateSpeechToken(reload = true) {
+    const context = new HttpContext()
+      .set(NO_LOADER_TOKEN, true)
+      .set(NO_ERROR_HANDLE, true);
+
     const url = this.urlService.AZURE_URLS.SPEECH_TOKEN;
     return this.http
-      .get<SpeechTokenContract>(url)
+      .get<SpeechTokenContract>(url, { context })
       .pipe(tap(res => this.appStore.updateSpeechToken(res)));
   }
 }
