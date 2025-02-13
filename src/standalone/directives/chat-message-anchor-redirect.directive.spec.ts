c7f769b4b450908da039a0284686710a4de19594
Ameer-Shahoud <ameer.shahoud.1997@gmail.com>
2025-02-13 14:08:17 +0300

    fixing multiple event handlers accumulated for chat links

---
+++ src/standalone/directives/chat-message-anchor-redirect.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ChatMessageAnchorRedirectDirective } from './chat-message-anchor-redirect.directive';
+
+describe('ChatMessageAnchorRedirectDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ChatMessageAnchorRedirectDirective();
+    expect(directive).toBeTruthy();
+  });
+});
