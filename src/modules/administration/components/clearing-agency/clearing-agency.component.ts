861434307ec0d141a8d83c40cd1bd1224e3070c1
aalshahin <aalshahin@eblacorp.com>
2025-02-23 15:59:52 +0400

    Bug 59945: Admin - clear agent companies - error when click on logs icon

--- src/modules/administration/components/clearing-agency/clearing-agency.component.ts
+++ src/modules/administration/components/clearing-agency/clearing-agency.component.ts
@@ -46,15 +46,6 @@ export class ClearingAgencyComponent
       },
     },
     {
-      name: 'audit',
-      type: 'action',
-      label: 'audit',
-      icon: AppIcons.HISTORY,
-      callback: item => {
-        this.viewAudit$.next(item);
-      },
-    },
-    {
       name: 'more-details',
       type: 'info',
       label: 'more_details',
