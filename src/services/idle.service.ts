40fad89f6957ae653a25f8a27826182e861bb6cd
Ameer-Shahoud <ameer.shahoud.1997@gmail.com>
2025-03-13 11:46:47 +0300

    remove idle logs

--- src/services/idle.service.ts
+++ src/services/idle.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { filter, interval, Subscription, tap } from 'rxjs';
+import { filter, interval, Subscription } from 'rxjs';
 import { AuthService } from './auth.service';
 import { InfoService } from './info.service';
 import { LangService } from './lang.service';
@@ -15,16 +15,13 @@ export class IdleService {
   private _idleIntervalSubscription?: Subscription;
 
   setIdleInterval() {
-    console.log('chech timeout: ', this._getSessionTimeOut());
     if (!this._getSessionTimeOut()) return;
     this._idleIntervalSubscription?.unsubscribe();
     this._idleIntervalSubscription = interval(
       this._getSessionTimeOut()! * 60 * 1000,
     )
-      .pipe(tap(() => console.log('session timeout')))
       .pipe(filter(() => this._auth.isAuthenticated()))
       .subscribe(() => {
-        console.log('session logout');
         this._idleIntervalSubscription?.unsubscribe();
         this._auth.logout(this._lang.map.session_is_over);
       });
